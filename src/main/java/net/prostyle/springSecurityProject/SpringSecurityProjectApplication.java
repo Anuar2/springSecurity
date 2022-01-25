4329e27c5900b022363858fba167acc3982f2e8b
Anuar2 <89724888+Anuar2@users.noreply.github.com>
2022-01-25 08:25:51 +0600

    springSecurity
    check

---
+++ src/main/java/net/prostyle/springSecurityProject/SpringSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package net.prostyle.springSecurityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProjectApplication.class, args);
+	}
+
+}
